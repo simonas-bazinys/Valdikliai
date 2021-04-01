59d9d9cadb45944c4b09e139efaf4aa7ed9fd56d
Simonas Bazinys <sbazinys@gmail.com>
2021-04-01 13:23:55 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,367 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2014 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stm32f4xx_hal.h>
+#include "diag/trace.h"
+
+#include "timer.h"
+#include "led.h"
+
+// ----------------------------------------------------------------------------
+//
+// Semihosting STM32F4 led blink sample (trace via DEBUG).
+//
+// In debug configurations, demonstrate how to print a greeting message
+// on the trace device. In release configurations the message is
+// simply discarded.
+//
+// To demonstrate semihosting, display a message on the standard output
+// and another message on the standard error.
+//
+// Then demonstrates how to blink a led with 1 Hz, using a
+// continuous loop and SysTick delays.
+//
+// On DEBUG, the uptime in seconds is also displayed on the trace device.
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the DEBUG output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace-impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// Definitions visible only within this translation unit.
+namespace
+{
+  // ----- Timing definitions -------------------------------------------------
+
+  // Keep the LED on for 2/3 of a second.
+  constexpr timer::ticks_t BLINK_ON_TICKS = timer::FREQUENCY_HZ * 3 / 4;
+  constexpr timer::ticks_t BLINK_OFF_TICKS = timer::FREQUENCY_HZ
+      - BLINK_ON_TICKS;
+}
+
+// ----- LED definitions ------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C"
+#endif
+
+
+#if defined(STM32F401xE)
+
+#warning "Assume a NUCLEO-F401RE board, PA5, active high."
+
+// PA5
+#define BLINK_PORT_NUMBER         (0)
+#define BLINK_PIN_NUMBER          (5)
+#define BLINK_ACTIVE_LOW          (false)
+
+led blinkLeds[1] =
+  {
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER, BLINK_ACTIVE_LOW },
+  };
+
+#elif defined(STM32F407xx)
+
+#warning "Assume a STM32F4-Discovery board, PD12-PD15, active high."
+
+#define BLINK_PORT_NUMBER         (3)
+#define BLINK_PIN_NUMBER_GREEN    (12)
+#define BLINK_PIN_NUMBER_ORANGE   (13)
+#define BLINK_PIN_NUMBER_RED      (14)
+#define BLINK_PIN_NUMBER_BLUE     (15)
+#define BLINK_ACTIVE_LOW          (false)
+
+led blinkLeds[4] =
+  {
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_GREEN, BLINK_ACTIVE_LOW },
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_ORANGE, BLINK_ACTIVE_LOW },
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_RED, BLINK_ACTIVE_LOW },
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_BLUE, BLINK_ACTIVE_LOW },
+  };
+
+#elif defined(STM32F411xE)
+
+#warning "Assume a NUCLEO-F411RE board, PA5, active high."
+
+#define BLINK_PORT_NUMBER         (0)
+#define BLINK_PIN_NUMBER          (5)
+#define BLINK_ACTIVE_LOW          (false)
+
+led blinkLeds[1] =
+  {
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER, BLINK_ACTIVE_LOW },
+  };
+
+#elif defined(STM32F429xx)
+
+#warning "Assume a STM32F429I-Discovery board, PG13-PG14, active high."
+
+#define BLINK_PORT_NUMBER         (6)
+#define BLINK_PIN_NUMBER_GREEN    (13)
+#define BLINK_PIN_NUMBER_RED      (14)
+#define BLINK_ACTIVE_LOW          (false)
+
+led blinkLeds[2] =
+  {
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_GREEN, BLINK_ACTIVE_LOW },
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER_RED, BLINK_ACTIVE_LOW },
+  };
+
+#else
+
+#warning "Unknown board, assume PA5, active high."
+
+#define BLINK_PORT_NUMBER         (0)
+#define BLINK_PIN_NUMBER          (5)
+#define BLINK_ACTIVE_LOW          (false)
+
+led blinkLeds[1] =
+  {
+    { BLINK_PORT_NUMBER, BLINK_PIN_NUMBER, BLINK_ACTIVE_LOW },
+  };
+
+#endif
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+void SysTick_Handler(void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
+static TIM_HandleTypeDef s_TimerInstance = {.Instance = TIM4};
+
+void initializeTimer()
+{
+	__TIM4_CLK_ENABLE();
+	s_TimerInstance.Init.Prescaler = 0;
+	s_TimerInstance.Init.CounterMode = TIM_COUNTERMODE_UP;
+	s_TimerInstance.Init.Period = 100;
+	s_TimerInstance.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	s_TimerInstance.Init.RepetitionCounter = 0;
+	HAL_TIM_Base_Init(&s_TimerInstance);
+	HAL_TIM_Base_Start(&s_TimerInstance);
+}
+
+void initializeLED()
+{
+	__GPIOD_CLK_ENABLE();
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.Pin = GPIO_PIN_13; //blue
+	GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStructure.Pull = GPIO_NOPULL;
+
+	GPIO_InitStructure.Speed = GPIO_SPEED_LOW;
+	GPIO_InitStructure.Alternate = GPIO_AF2_TIM4;
+
+	HAL_GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+void initializeLED2()
+{
+	__GPIOD_CLK_ENABLE();
+
+		GPIO_InitTypeDef GPIO_InitStructure2;
+		GPIO_InitStructure2.Pin = GPIO_PIN_15; //blue
+		GPIO_InitStructure2.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStructure2.Pull = GPIO_NOPULL;
+
+		GPIO_InitStructure2.Speed = GPIO_SPEED_LOW;
+		GPIO_InitStructure2.Alternate = GPIO_AF2_TIM4;
+
+		HAL_GPIO_Init(GPIOD, &GPIO_InitStructure2);
+}
+
+void initializeButton()
+{
+	__GPIOD_CLK_ENABLE();
+
+	GPIO_InitTypeDef GPIO_InitStructure3;
+	GPIO_InitStructure3.Pin = GPIO_PIN_0;
+	GPIO_InitStructure3.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStructure3.Pull = GPIO_PULLDOWN;
+
+	GPIO_InitStructure3.Speed = GPIO_SPEED_LOW;
+	GPIO_InitStructure3.Alternate = GPIO_AF2_TIM4;
+
+	HAL_GPIO_Init(GPIOD, &GPIO_InitStructure3);
+}
+
+void InitializeTimerPWM(int brightness)
+{
+	TIM_OC_InitTypeDef sConfigOC;
+
+	HAL_TIM_PWM_Init(&s_TimerInstance);
+
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	sConfigOC.Pulse = brightness;
+	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	sConfigOC.OCFastMode = TIM_OCFAST_ENABLE;
+
+	HAL_TIM_PWM_ConfigChannel(&s_TimerInstance, &sConfigOC, TIM_CHANNEL_4);
+	HAL_TIM_PWM_Start(&s_TimerInstance, TIM_CHANNEL_4);
+
+
+}
+int
+main(int argc, char* argv[])
+{
+	  // Show the program parameters (passed via semihosting).
+	  // Output is via the semihosting output channel.
+	  trace_dump_args(argc, argv);
+
+	  // Send a greeting to the trace device (skipped on Release).
+	  trace_puts("Hello Arm World!");
+
+	  // Send a message to the standard output.
+	  puts("Standard output message.");
+
+	  // Send a message to the standard error.
+	  fprintf(stderr, "Standard error message.\n");
+
+	  // At this stage the system clock should have already been configured
+	  // at high speed.
+	  trace_printf("System clock: %u Hz\n", SystemCoreClock);
+
+	  timer timer;
+	  timer.start ();
+
+	  // Perform all necessary initialisations for the LEDs.
+	  for (size_t i = 0; i < (sizeof(blinkLeds) / sizeof(blinkLeds[0])); ++i)
+	    {
+	      blinkLeds[i].power_up ();
+	    }
+
+	  uint32_t seconds = 0;
+
+	#define LOOP_COUNT (1 << (sizeof(blinkLeds) / sizeof(blinkLeds[0])))
+
+	  int loops = LOOP_COUNT > 2 ? LOOP_COUNT : (5);
+	  if (argc > 1)
+	    {
+	      // If defined, get the number of loops from the command line,
+	      // configurable via semihosting.
+	      loops = atoi (argv[1]);
+	      if (loops < LOOP_COUNT)
+	        {
+	          loops = LOOP_COUNT;
+	        }
+	    }
+
+	  for (size_t i = 0; i < (sizeof(blinkLeds) / sizeof(blinkLeds[0])); ++i)
+	    {
+	      blinkLeds[i].turn_on ();
+	    }
+
+	  // First second is long.
+	  timer.sleep (timer::FREQUENCY_HZ);
+
+	  for (size_t i = 0; i < (sizeof(blinkLeds) / sizeof(blinkLeds[0])); ++i)
+	    {
+	      blinkLeds[i].turn_off ();
+	    }
+
+	  timer.sleep (BLINK_OFF_TICKS);
+
+	  ++seconds;
+	  trace_printf ("Second %u\n", seconds);
+
+	  if ((sizeof(blinkLeds) / sizeof(blinkLeds[0])) > 1)
+	    {
+	      // Blink individual LEDs.
+	      for (size_t i = 0; i < (sizeof(blinkLeds) / sizeof(blinkLeds[0])); ++i)
+	        {
+	          blinkLeds[i].turn_on ();
+	          timer.sleep (BLINK_ON_TICKS);
+
+	          blinkLeds[i].turn_off ();
+	          timer.sleep (BLINK_OFF_TICKS);
+
+	          ++seconds;
+	          trace_printf ("Second %u\n", seconds);
+	        }
+
+	      // Blink binary.
+	      for (int i = 0; i < loops; i++)
+	        {
+	          for (size_t l = 0; l < (sizeof(blinkLeds) / sizeof(blinkLeds[0]));
+	              ++l)
+	            {
+	              blinkLeds[l].toggle ();
+	              if (blinkLeds[l].isOn ())
+	                {
+	                  break;
+	                }
+	            }
+	          timer.sleep (timer::FREQUENCY_HZ);
+
+	          ++seconds;
+	          trace_printf ("Second %u\n", seconds);
+	        }
+	    }
+	  else
+	    {
+	      for (int i = 0; i < loops; i++)
+	        {
+	          blinkLeds[0].turn_on ();
+	          timer.sleep (BLINK_ON_TICKS);
+
+	          blinkLeds[0].turn_off ();
+	          timer.sleep (BLINK_OFF_TICKS);
+
+	          ++seconds;
+	          trace_printf ("Second %u\n", seconds);
+	        }
+	    }
+
+	  for (size_t i = 0; i < (sizeof(blinkLeds) / sizeof(blinkLeds[0])); ++i)
+	    {
+	      blinkLeds[i].turn_on ();
+	    }
+
+	  timer.sleep (timer::FREQUENCY_HZ);
+
+	  return 0;}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
